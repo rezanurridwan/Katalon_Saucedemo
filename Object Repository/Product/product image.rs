4ebea825f85b13f5b6f51246143fd10f588c8327
reza nur ridwan <jeypecker01@gmail.com>
2024-02-22 09:19:07 +0700

    create test case for description product

---
+++ Object Repository/Product/product image.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>product image</name>
+   <tag></tag>
+   <elementGuidId>1c92e0a8-0ecf-4e10-abd7-8c69f053f953</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//img[@alt='Sauce Labs Backpack']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Product/product title.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>product title</name>
+   <tag></tag>
+   <elementGuidId>46173979-bc7a-426b-9e4d-a4fea95ec85d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[normalize-space()='Sauce Labs Backpack'] </value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
